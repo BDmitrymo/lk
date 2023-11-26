ad34cd01a486b49627dc37d5d1427ba581d6fcb6
BDmitrymo <dbatkovmo@gmail.com>
2023-11-27 00:04:13 +0300

    Update

--- src/components/admin/MenuAdmin/MenuAdmin.tsx
+++ src/components/admin/MenuAdmin/MenuAdmin.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {ReactComponent as Logo} from '../../../assets/img/logo.svg';
 import './MenuAdmin.scss'
+import {Button} from 'antd';
+
 
 export const MenuAdmin = () => {
   return (
     <div className="as__admin-menu">
-      Menu
+      <Logo className="as__logo"/>
+      <div className="as__admin-menu_top">
+        <button className="as__admin-menu_button">
+          <i className="fa-solid fa-house"/>
+        </button>
+      </div>
     </div>
   );
 };
