fb0847a95dd16d233234558b08695835d7dee2f2
BDmitrymo <dbatkovmo@gmail.com>
2023-11-27 22:54:12 +0300

    Update

--- src/components/admin/HeaderAdmin/HeaderAdmin.tsx
+++ src/components/admin/HeaderAdmin/HeaderAdmin.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Flex} from 'antd';
-import {ProfileUser} from '../../ui/ProfileUser/ProfileUser';
-import {SwitchLang} from '../../ui/SwitchLang/SwitchLang';
-import './HeaderAdmin.scss';
+import React from "react";
+import { Flex } from "antd";
+import { ProfileUser } from "@/components/ui/ProfileUser/ProfileUser";
+import { SwitchLang } from "@/components/ui/SwitchLang/SwitchLang";
+import "./HeaderAdmin.scss";
 
 export const HeaderAdmin = () => {
   return (
-    <Flex className="as__admin-header" style={{height: '100%'}} justify="flex-end" align="center">
-      <SwitchLang/>
-      <ProfileUser/>
+    <Flex className="as__admin-header" style={{ height: "100%" }} justify="flex-end" align="center">
+      <SwitchLang />
+      <ProfileUser />
     </Flex>
   );
 };
